e05bd8fe580b061d16451215e770e20a540ee383
impronez <orzaev7312@gmail.com>
2025-02-19 13:09:10 +0300

    Создал сервис хранилища

---
+++ Valuator/Services/IStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace Valuator.Services
+{
+	public interface IStorageService
+	{
+		public void SetValue(string key, string value);
+		public string? GetValue(string key);
+		public List<string> GetAllValuesByKeyPrefix(string keyPrefix);
+	}
+}
---
+++ Valuator/Services/RedisStorageService.cs
@@ -0,0 +1,38 @@
+﻿using StackExchange.Redis;
+
+namespace Valuator.Services
+{
+	public class RedisStorageService : IStorageService
+	{
+		public readonly IDatabase _database;
+
+		public RedisStorageService(IConnectionMultiplexer redis)
+		{
+			_database = redis.GetDatabase();
+		}
+
+		public void SetValue(string key, string value)
+		{
+			_database.StringSet(key, value);
+		}
+
+		public string? GetValue(string key)
+		{
+			return _database.StringGet(key);
+		}
+
+		public List<string> GetAllValuesByKeyPrefix(string keyPrefix)
+		{
+			var values = new List<string>();
+
+			var textsKeys = (RedisResult[])_database.Execute("KEYS", $"{keyPrefix}-*");
+
+			foreach (RedisResult key in textsKeys)
+			{
+				values.Add(GetValue(key.ToString()));
+			}
+
+			return values;
+		}
+	}
+}
